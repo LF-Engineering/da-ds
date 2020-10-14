3088365e518200f13a7d2685d213fb031b771291
Łukasz Gryglicki <lukaszgryglicki@o2.pl>
2020-10-14 14:49:01 +0000

    Update groups.io version to 0.1.0 - the first which works
    Signed-off-by: Łukasz Gryglicki <lukaszgryglicki@o2.pl>

--- dsgroupsio.go
+++ dsgroupsio.go
@@ -20,7 +20,7 @@ import (
 
 const (
 	// GroupsioBackendVersion - backend version
-	GroupsioBackendVersion = "0.0.0"
+	GroupsioBackendVersion = "0.1.0"
 	// GroupsioURLRoot - root url for group name origin
 	GroupsioURLRoot = "https://groups.io/g/"
 	// GroupsioAPIURL - Groups.io API URL
