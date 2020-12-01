031a5288da49b90bca39c8d94d7054d97e71fcf9
Ayman <enkhalifapro@gmail.com>
2020-12-01 12:33:17 +0200

    DA-2812: fix lint errors

--- dockerhub/fetcher.go
+++ dockerhub/fetcher.go
@@ -94,7 +94,7 @@ func (f *Fetcher) Login(username string, password string) (string, error) {
 	return "", errors.New("invalid login credentials")
 }
 
-// FetchItems ...
+// FetchItem pulls image data
 func (f *Fetcher) FetchItem(owner string, repository string, now time.Time) (*RepositoryRaw, error) {
 	requestURL := fmt.Sprintf("%s/%s/%s/%s/%s", APIURL, APIVersion, APIRepositories, owner, repository)
 	url := fmt.Sprintf("%s/%s/%s", APIURL, owner, repository)
