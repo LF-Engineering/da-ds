2d87d651393d024e424de3fcacbe076f46fdbf6c
Ibrahim Mbaziira <code.ibra@gmail.com>
2021-01-25 16:01:25 +0300

    add shell script that will run the scraper

---
+++ googlegroups/cmd.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+setUp(){
+
+venv=/Users/code/da-ds/googlegroups/venv
+workDir=/Users/code/da-ds/googlegroups/archives
+jsonFilesDir=/Users/code/da-ds/googlegroups/jsonfiles
+mboxScript=/Users/code/da-ds/googlegroups/ggmbox.py
+logsDir=/Users/code/da-ds/googlegroups/logs/
+
+cd $workDir || exit
+
+jsonFile=${jsonFilesDir}"/""$1".json
+current_time=$(date "+%Y.%m.%d-%H.%M.%S")
+logfile=${logsDir}"$1".${current_time}.txt
+
+if [ -e "$venv" ]; then
+  source $venv/bin/activate
+  scrapy runspider -a name="$1" -o "${jsonFile}" -t json ${mboxScript} > "${logfile}" 2>&1
+else
+  virtualenv --python=python3 $venv
+  source $venv/bin/activate
+  pip install scrapy
+  scrapy runspider -a name="$1" -o "${jsonFile}" -t json ${mboxScript} > "${logfile}" 2>&1
+fi
+}
+
+main(){
+    setUp "$1"
+}
+
+main "$@"
