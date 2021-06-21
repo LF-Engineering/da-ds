b3e1466eab6bd62c25c77cc70069bf97bbc150ba
“khalifapro” <enkhalifapro@yahoo.com>
2021-06-21 13:08:24 +0200

    add mail check to bugzilla

--- util/helper.go
+++ util/helper.go
@@ -8,6 +8,7 @@ import (
 
 var emailRegex = regexp.MustCompile(`^[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,4}$`)
 
+// IsEmailValid  validate email address
 func IsEmailValid(e string) bool {
 	if len(e) < 3 && len(e) > 254 {
 		return false
